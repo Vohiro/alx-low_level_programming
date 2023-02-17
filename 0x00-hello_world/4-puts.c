bcb1650fcefe019815ec0d72093228dccd48f55b
Vohiro <dvohiro@gmail.com>
2023-02-16 16:23:45 -0800

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program is weird
+ *
+ * Return: return 0 means success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
