2370e597d18a0ab83f45a981ebbb2f95db5c6537
Vohiro <dvohiro@gmail.com>
2023-02-16 17:47:16 -0800

    over

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  *
  * Description: using the main function
  * this program prints "and that piece of art is useful - Dora Korpar
- * Return: This time we return an error (1)
+ * Return: 0
  */
 int main(void)
 {
