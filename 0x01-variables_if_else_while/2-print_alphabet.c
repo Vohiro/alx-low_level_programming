847a0fc4e0265532e1579fc252866827b5c4af98
Vohiro <dvohiro@gmail.com>
2023-02-17 12:58:04 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - alphabets lowercase
+ * Return: 0 equal success
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while 
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
