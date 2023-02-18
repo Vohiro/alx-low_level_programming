7832e4577a10a93eae5866b0f3c2fce6f0d0db85
Vohiro <dvohiro@gmail.com>
2023-02-17 17:40:34 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - upper and lower case
+ * Return: 0 equal sucess
+ */
+
+int main(void)
+{
+	char x;
+
+	char y;
+
+	x = 'a';
+	y = 'A';
+	while
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	while
+		(y <= 'Z') {
+			putchar(y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}
