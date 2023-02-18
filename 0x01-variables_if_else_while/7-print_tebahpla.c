dbb5e3f3d7fb42979b768a6b4007f4989c7ebe73
Vohiro <dvohiro@gmail.com>
2023-02-17 19:08:56 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a') {
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
