f9066adb6ec2022b088570feeaed74d1d9218968
Vohiro <dvohiro@gmail.com>
2023-02-17 18:26:51 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ * Return: 0 equal success
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	putchar('\n');
+	return (0);
+}
