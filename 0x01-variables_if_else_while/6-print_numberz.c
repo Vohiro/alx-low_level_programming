890b93168c543b94e865f0fff4775add04cb8f25
Vohiro <dvohiro@gmail.com>
2023-02-17 18:50:23 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ * Return: 0 = complete
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while
+		(x < 10) {
+			putchar(x + '0');
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
