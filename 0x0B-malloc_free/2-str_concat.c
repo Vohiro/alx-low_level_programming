e2c392437c50b57a7e53e3222ea1937365fa4530
Vohiro <dvohiro@gmail.com>
2023-03-13 15:41:00 -0700

    error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,51 @@
 #include "main.h"
+#include <string.h>
 #include <stdlib.h>
 
 /**
  * str_concat - get ends of input and add together for size
  * @s1: input one to concat
  * @s2: input two to concat
- * Return: concat of s1 and s2
+ * Return: NULL in case of failure, but pointer to new
+ * string in case of success
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	int i, j;
+	int len1, len2;
+	char *ptr;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 
 	if (s2 == NULL)
+	{
 		s2 = "";
-		i = ci = 0;
+	}
 
-	while (s1[i] != '\0')
-		i++;
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-	if (conct == NULL)
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (ptr == NULL)
+	{
 		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
 	}
-	while (s2[ci] != '\0')
+
+	for (i = 0; i < len1; i++)
 	{
-		conct[i] = s2[ci];
-		i++, ci++;
+		ptr[i] = s1[i];
 	}
-	conct[i] = '\0';
-	return (conct);
+
+	for (j = 0; j < len2; j++)
+	{
+		ptr[len1 + j] = s2[j];
+	}
+
+	ptr[len1 + len2] = '\0';
+	return (ptr);
 }
