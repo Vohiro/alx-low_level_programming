af6066067e38c88c5f4f2ef3299b5fd8ff21a137
Vohiro <dvohiro@gmail.com>
2023-08-21 15:22:24 -0700

    advance

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include "100-main.h"
+/**
+* add - function that returns sum of 2 integers.
+* @a: first int
+* @b: second int
+* Return: Sum of a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* sub - function that returns difference of 2 integers.
+* @a: first int
+* @b: second int
+* Return: Difference of a and b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* mul - function that multiplys 2 integers.
+* @a: first int
+* @b: second int
+* Return: a * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* div - function that returns sum of 2 integers.
+* @a: first int
+* @b: second int
+* Return: a divided by b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* mod - function that returns a mod b.
+* @a: first int
+* @b: second int
+* Return: a modulo b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
